06c5563f7d3377d368901c8f98f84715395fdade
0n1dev <all_mind@naver.com>
2022-01-26 22:52:26 +0900

    feat: Logout

--- src/main/java/com/example/springsecuritypractice/SecurityConfig.java
+++ src/main/java/com/example/springsecuritypractice/SecurityConfig.java
@@ -8,10 +8,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Configuration
@@ -46,5 +49,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     }
                 }) // 로그인 실패 핸들러
                 .permitAll(); // 누구드닞 접근 가능하도록 권한 설정.
+
+        http.logout() // 로그아웃 처리
+                .logoutUrl("/logout") // 로그아웃 처리 URL
+                .logoutSuccessUrl("/login") // 로그아웃 성공 후 이동 페이지
+                .addLogoutHandler(new LogoutHandler() {
+                    @Override
+                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+                        HttpSession session = request.getSession();
+                        session.invalidate();
+                    }
+                }) // 로그아웃 핸들러
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        response.sendRedirect("/login");
+                    }
+                }) // 로그아웃 성공 후 핸들러러
+                .deleteCookies("JESSIONID", "remember-me"); // 로그아웃 후 쿠키 삭제
     }
+
 }
